8289e4c6e7fa5d415c35f0414007bc04dd773215
Denys Matsenko <idanchik47@gmail.com>
2020-09-04 22:25:45 +0300

    project is configured

---
+++ src/main/java/com/github/saintukrainian/filmsfinder/FilmsFinderApplication.java
@@ -0,0 +1,13 @@
+package com.github.saintukrainian.filmsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/saintukrainian/filmsfinder/FilmsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.saintukrainian.filmsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
