9dde024e53667d6d605193d259ed44c180230ce1
Denys Matsenko <idanchik47@gmail.com>
2020-09-07 10:03:39 +0300

    ...

--- src/main/resources/static/scripts/home.js
+++ src/main/resources/static/scripts/home.js
@@ -3,13 +3,13 @@ const genresListHtml = document.querySelector(".genres-list");
 const movieList = document.getElementById("movie-list");
 const backdrop = document.querySelector(".backdrop");
 const searchForm = document.getElementById("search-film");
+const filmsSection = document.querySelector(".films");
 
 // temporarily like that :)
 const hrefFilmsUrl = `http://localhost:8080/#films-section`;
-const hrefGenresUrl = `http://localhost:8080/#genres-section`
+const hrefGenresUrl = `http://localhost:8080/#genres-section`;
 const postFilmsUrl = `http://localhost:8080/films`;
 const genresHtml = document.querySelectorAll(".genre-btn");
-
 
 let filmsByGenre = [];
 
@@ -17,7 +17,11 @@ async function addListenerToGenres() {
     for (const genreItem of genresHtml) {
         genreItem.addEventListener("click", async (event) => {
             location.href = hrefGenresUrl;
-            document.querySelector(".films").classList.remove("visible");
+            setTimeout(() => {
+                if (filmsSection.classList.contains("visible")) {
+                    filmsSection.classList.remove("visible");
+                }
+            }, 300);
             backdrop.classList.add("visible");
             movieList.innerHTML = "";
             const id = event.target.closest("li").getAttribute("id");
@@ -39,7 +43,9 @@ async function addListenerToGenres() {
                 const filmElement = document.createElement("li");
                 filmElement.className = "movie-element";
                 filmElement.setAttribute("id", film.id);
-                let imageUrl = film.poster_path ? `https://image.tmdb.org/t/p/original${film.poster_path}` : "https://cdn.browshot.com/static/images/not-found.png";
+                let imageUrl = film.poster_path
+                    ? `https://image.tmdb.org/t/p/original${film.poster_path}`
+                    : "https://cdn.browshot.com/static/images/not-found.png";
                 filmElement.innerHTML = `
                     <div class="movie-element__image">
                         <img src="${imageUrl}" alt="${film.title}">
@@ -57,7 +63,7 @@ async function addListenerToGenres() {
                 axios.post(postFilmsUrl, filmsJava);
                 location.href = hrefFilmsUrl;
                 backdrop.classList.remove("visible");
-                document.querySelector(".films").classList.add("visible");
+                filmsSection.classList.add("visible");
             }, 2000);
         });
     }
@@ -66,19 +72,64 @@ async function addListenerToGenres() {
 addListenerToGenres();
 
 searchForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    backdrop.classList.add("visible");
+    location.href = hrefGenresUrl;
+    setTimeout(() => {
+        if (filmsSection.classList.contains("visible")) {
+            filmsSection.classList.remove("visible");
+        }
+    }, 200);
     const input = document.getElementById("film-title");
     const regex = /,\s|\s/;
-    event.preventDefault();
     console.log("Searching for film with title = " + input.value);
-    const title = input.value.toLowerCase().split(regex).join("+");
+    const title = input.value.trim().toLowerCase().split(regex).join("+");
     console.log(title);
-    const filmsByTitle = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=58af3dc3b19432c261816f7a48688477&query=${title}`).then(response => response.data.results);
+    const filmsByTitle = await axios
+        .get(
+            `https://api.themoviedb.org/3/search/movie?api_key=58af3dc3b19432c261816f7a48688477&query=${title}`
+        )
+        .then((response) => response.data.results);
     let foundFilm;
     for (const film of filmsByTitle) {
-        if(film.original_title.toLowerCase().split(regex).join("+") === title){
+        if (
+            film.original_title.toLowerCase().split(regex).join("+") === title
+        ) {
             foundFilm = film;
             break;
         }
     }
-    console.log(foundFilm ? foundFilm : "sorry, we couldn't find this film");
+
+    if (!foundFilm) {
+        movieList.innerHTML = `<h1>Sorry, we couldn't find the film with title: ${input.value}</h1>`;
+        setTimeout(() => {
+            filmsSection.classList.add("visible");
+            backdrop.classList.remove("visible");
+            location.href = hrefFilmsUrl;
+        }, 1000);
+    } else {
+        movieList.innerHTML = "";
+        const filmElement = document.createElement("li");
+        filmElement.className = "searched-element";
+        let imageUrl = foundFilm.poster_path
+            ? `https://image.tmdb.org/t/p/original${foundFilm.poster_path}`
+            : "https://cdn.browshot.com/static/images/not-found.png";
+        filmElement.innerHTML = `
+            <div class="searched-element__image">
+                <img src="${imageUrl}" alt="${foundFilm.title}">
+            </div>
+            <div class="searched-element__info">
+                <h5 id="title">${foundFilm.title}</h5>
+                <hr class="hr-list">
+                <p class="searched-info"><span>Release date: </span>${foundFilm.release_date}</p>
+                <p class="searched-info"><span>Plot: </span>${foundFilm.overview}</p>
+           </div>
+        `;
+        movieList.appendChild(filmElement);
+        setTimeout(() => {
+            filmsSection.classList.add("visible");
+            backdrop.classList.remove("visible");
+            location.href = hrefFilmsUrl;
+        }, 1000);
+    }
 });
